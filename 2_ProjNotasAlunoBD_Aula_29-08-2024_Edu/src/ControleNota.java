ffec3bd36a12c9e453e058c805392614c73d8082
pecosta23 <pedrocosta2802@gmail.com>
2024-09-04 22:58:29 -0300

    Adciona lição em Java

--- 2_ProjNotasAlunoBD_Aula_29-08-2024_Edu/src/ControleNota.java
+++ 2_ProjNotasAlunoBD_Aula_29-08-2024_Edu/src/ControleNota.java
@@ -1,8 +1,38 @@
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
 
 public class ControleNota {
+    private Connection conexao;
+    private Statement comando;
+    private PreparedStatement sql;
+    private ResultSet lista;
     
-    
-        public void Conexao(){
-            System.out.println("ola");
+    public void Conexao(){
+        //criacao das cariaveis para acesso ao mysql
+        String nomeservidor = "Localhost:3306";
+        String usuario = "root";
+        String senha = "";
+        String nomebanco = "bdalunos";
+        // variavel que contem o nome do driver
+        String drivername="com.mysql.cj.jdbc.Driver";
+        //ativando o driver
+        try{
+            //ativar o driver
+            Class.forName(drivername);
+            //criando a conexao ao banco de dados
+            String url="jdbc:mysql://"+nomeservidor+"/"+nomebanco+"?useTimezone=true&serverTimezone=UTC";
+            conexao = DriverManager.getConnection(url, usuario, senha);
+            comando = conexao.createStatement();
         }
+        catch (ClassNotFoundException e){
+            JOptionPane.showMessageDialog(null, "Erro de driver");
+        }
+        catch (Exception e){
+            JOptionPane.showMessageDialog(null, "Erro ao conectar ao banco de dados");
+        }    
+    }      
 }
--- 2_ProjNotasAlunoBD_Aula_29-08-2024_Edu/src/FormNota.java
+++ 2_ProjNotasAlunoBD_Aula_29-08-2024_Edu/src/FormNota.java
@@ -29,21 +29,166 @@ public class FormNota extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
+        textrgm = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        textnome = new javax.swing.JTextField();
+        jLabel3 = new javax.swing.JLabel();
+        textnota1 = new javax.swing.JTextField();
+        jLabel4 = new javax.swing.JLabel();
+        textnota2 = new javax.swing.JTextField();
+        butCadastrar = new javax.swing.JButton();
+        butListar = new javax.swing.JButton();
+        butConsultar = new javax.swing.JButton();
+        butAlterar = new javax.swing.JButton();
+        butSair = new javax.swing.JButton();
+        textNotasAlunos = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setText("RGM");
+
+        textrgm.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                textrgmActionPerformed(evt);
+            }
+        });
+
+        jLabel2.setText("Nome do Aluno");
+
+        jLabel3.setText("Nota 1");
+
+        textnota1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                textnota1ActionPerformed(evt);
+            }
+        });
+
+        jLabel4.setText("Nota 2");
+
+        butCadastrar.setText("Cadastrar");
+
+        butListar.setText("Listar");
+        butListar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                butListarActionPerformed(evt);
+            }
+        });
+
+        butConsultar.setText("Consultar");
+
+        butAlterar.setText("Alterar");
+        butAlterar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                butAlterarActionPerformed(evt);
+            }
+        });
+
+        butSair.setText("Sair");
+        butSair.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                butSairActionPerformed(evt);
+            }
+        });
+
+        textNotasAlunos.setText("Notas de Alunos");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(25, 25, 25)
+                        .addComponent(butCadastrar)
+                        .addGap(18, 18, 18)
+                        .addComponent(butListar, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(butConsultar, javax.swing.GroupLayout.PREFERRED_SIZE, 88, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(butAlterar, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(butSair, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(16, 16, 16)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jLabel4)
+                            .addComponent(jLabel3)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel1)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(textrgm, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel2)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(textnota1, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(textnome, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(textnota2, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE))))))
+                .addContainerGap(21, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(textNotasAlunos)
+                .addGap(224, 224, 224))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(23, 23, 23)
+                .addComponent(textNotasAlunos)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(textrgm, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel2)
+                            .addComponent(textnome, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(24, 24, 24)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel3)
+                            .addComponent(textnota1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(18, 18, 18)
+                        .addComponent(jLabel4))
+                    .addComponent(textnota2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 129, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(butCadastrar)
+                    .addComponent(butListar)
+                    .addComponent(butConsultar)
+                    .addComponent(butAlterar)
+                    .addComponent(butSair))
+                .addGap(23, 23, 23))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void textrgmActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_textrgmActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_textrgmActionPerformed
+
+    private void textnota1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_textnota1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_textnota1ActionPerformed
+
+    private void butListarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_butListarActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_butListarActionPerformed
+
+    private void butAlterarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_butAlterarActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_butAlterarActionPerformed
+
+    private void butSairActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_butSairActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_butSairActionPerformed
 
     /**
      * @param args the command line arguments
@@ -81,5 +226,19 @@ public class FormNota extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton butAlterar;
+    private javax.swing.JButton butCadastrar;
+    private javax.swing.JButton butConsultar;
+    private javax.swing.JButton butListar;
+    private javax.swing.JButton butSair;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel textNotasAlunos;
+    private javax.swing.JTextField textnome;
+    private javax.swing.JTextField textnota1;
+    private javax.swing.JTextField textnota2;
+    private javax.swing.JTextField textrgm;
     // End of variables declaration//GEN-END:variables
 }
